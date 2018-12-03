bf1681a558e0bc2ce69b9c3b213c94dc205e7233
JSegovia <jsegovia@umail.ucsb.edu>
2018-12-02 23:35:06 -0800

    Added function stubs and separated out the jdbc connocter code to a helper class

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs.JonathanSegoviaArturoMilanes;
 //STEP 1. Import required packages
-import java.sql.*;
+//import java.sql.*;
 
 public class Main {
     // JDBC driver name and database URL
@@ -12,58 +12,10 @@ public class Main {
     static final String PASSWORD = "7654999";
 
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-            //STEP 2: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
-
-            //STEP 3: Open a connection
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            System.out.println("Connected database successfully...");
-
-            //STEP 4: Execute a query
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-
-            String sql = "SELECT cid, cname, city, discount FROM cs174.Customers";
-            ResultSet rs = stmt.executeQuery(sql);
-            //STEP 5: Extract data from result set
-            while(rs.next()){
-                //Retrieve by column name
-                String cid  = rs.getString("cid");
-                String cname = rs.getString("cname");
-                String city = rs.getString("city");
-                double discount = rs.getDouble("discount");
-
-                //Display values
-                System.out.print("cid: " + cid);
-                System.out.print(", cname: " + cname);
-                System.out.print(", city: " + city);
-                System.out.println(", discount: " + discount);
-            }
-            rs.close();
-        }catch(SQLException se){
-            //Handle errors for JDBC
-            se.printStackTrace();
-        }catch(Exception e){
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        }finally{
-            //finally block used to close resources
-            try{
-                if(stmt!=null)
-                    conn.close();
-            }catch(SQLException se){
-            }// do nothing
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
+        DBHelper dbHelper = new DBHelper();
+        dbHelper.connect("oracle.jdbc.driver.OracleDriver",
+                        "jdbc:oracle:thin:@cloud-34-133.eci.ucsb.edu:1521:XE",
+                        "jsegovia",
+                        "7654999");
     }//end main
 }//end JDBCExample
